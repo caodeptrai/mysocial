737a647351ddfb4411220720153fefba1f08b993
Duong Cao <duongvancao999@gmail.com>
2023-02-23 17:19:02 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -41,6 +41,7 @@ function App() {
                     element: <Register />,
                 },
                 {
+                    path: '/',
                     element: <ProtectedRoute />,
                     children: [
                         {
